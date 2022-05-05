4458434d8edcff1695a25aac59734f820366f614
Elena Martynova <63790124+marrtynova@users.noreply.github.com>
2022-05-05 17:19:21 +0300

    added task about animals

---
+++ Devico_test_task3/src/main/java/models/AnimalModel.java
@@ -0,0 +1,12 @@
+package models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnimalModel {
+    private String pigs;
+    private String chickens;
+    private String cows;
+}
